76fd4e1da158e5548aa647f6c24a667a673566bc
Travis Bozic <travis.bozic@hotmail.com>
2026-05-20 21:15:17 +1000

    feat(workflows): add webhook scripts and modify webhook trigger sub types

---
+++ webhook-trigger.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# curl -X POST \
+#   -H "Authorization: token YOUR_GITHUB_TOKEN" \
+#   -H "Accept: application/vnd.github+json" \
+#   https://api.github.com/repos/YOUR_ORG/YOUR_REPO/dispatches \
+#   -d '{"event_type": "webhook", "client_payload": {"key": "value"}}'
+
+curl -X POST \
+  -H "Authorization: token $API_TOKEN" \
+  -H "Accept: application/vnd.github+json" \
+  https://api.github.com/repos/travboz/exampro-github-actions-examples/dispatches \
+  -d '{"event_type": "", "client_payload": {"name": "Travis"}}'
