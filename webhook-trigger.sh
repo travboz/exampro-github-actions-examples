27e8a7324bd1ad00a116102d4fb3d957f4601305
Travis Bozic <travis.bozic@hotmail.com>
2026-05-20 21:17:20 +1000

    fix(workflows): change auth prefix from 'token' to 'Bearer'

--- webhook-trigger.sh
+++ webhook-trigger.sh
@@ -7,7 +7,7 @@
 #   -d '{"event_type": "webhook", "client_payload": {"key": "value"}}'
 
 curl -X POST \
-  -H "Authorization: token $API_TOKEN" \
   -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $API_TOKEN" \
   https://api.github.com/repos/travboz/exampro-github-actions-examples/dispatches \
   -d '{"event_type": "", "client_payload": {"name": "Travis"}}'
